8db64f50a94ab6146dddc0372febd60f699d6fc0
Abdulrahman muhammad muhammad <100371468+0xlegacy52@users.noreply.github.com>
2024-09-28 21:39:47 +0300

    Update legacyArsenal.sh

--- legacyArsenal.sh
+++ legacyArsenal.sh
@@ -1,186 +1,157 @@
-#!/usr/bin/env bash
-
-set -e  # Exit on error
-
+#!/bin/bash
 # Check if the script is run with root privileges
 if [ "$(id -u)" -ne "0" ]; then
     echo "This script must be run as root. Use 'sudo'."
     exit 1
 fi
 
-# Function to update and install necessary packages
-install_packages() {
-    echo "Updating package list and upgrading installed packages..."
-    apt update && apt upgrade -y
+mkdir -p ~/Desktop/Tools
+mkdir ~/.gf
+cd ~/Desktop/Tools
 
-    echo "Installing necessary packages from Kali repositories..."
-    apt-get install -y \
-        software-properties-common unzip wget make python3-pip build-essential git ruby-full \
-        python3 libpcap-dev cargo \
-        sublist3r assetfinder altdns dirsearch feroxbuster ffuf gobuster dirb dirbuster \
-        nmap nikto massdns wpscan dnsrecon jq \
-        golang-go  # Install Go from Kali's repository
-}
-# Create a virtual environment
-python3 -m venv ~/my-venv
+echo "Updating package list and upgrading installed packages..."
+apt update && apt upgrade -y && apt full-upgrade -y
 
-# Activate the virtual environment
-source ~/my-venv/bin/activate
+echo "Installing necessary packages..."
+apt-get install -y software-properties-common unzip wget make python3-pip build-essential git ruby-full python3 libpcap-dev cargo dotnet-host netstandard-targeting-pack-2.1 sublist3r assetfinder altdns dirsearch feroxbuster ffuf gobuster dirb dirbuster nmap nikto massdns wpscan dnsrecon jq amass screen ruby
+pip3 install arjun corscanner --break-system-packages
 
-# Function to set Go environment variables
-set_go_env() {
-    echo "Setting Go environment variables..."
-    export GOPATH=$HOME/go
-    export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:/root/go/bin:/$HOME/go/bin
-    echo "export GOPATH=$HOME/go" >> ~/.bashrc
-    echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin:/root/go/bin:/$HOME/go/bin" >> ~/.bashrc
-    source ~/.bashrc
-}
+echo "Adding Go PPA and installing Go..."
+add-apt-repository -y ppa:longsleep/golang-backports
+apt update
+apt install -y golang-go
 
-# Function to create tools directory and install pdtm
-install_pdtm() {
-    echo "Creating tools directory..."
-    mkdir -p ~/Desktop/tools
-    cd ~/Desktop/tools || exit
-    echo "Installing pdtm..."
-    go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
-    pdtm -ia
-}
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:/root/go/bin:/$HOME/go/bin
+echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin:/root/go/bin:/$HOME/go/bin" >> ~/.bashrc
+source ~/.bashrc
 
-# Function to download, unzip, and move binaries (for manually downloaded tools)
-install_tool() {
-    local url=$1
-    local filename=$(basename "$url")
-    local binname=${filename%%_*}
+mkdir -p ~/Desktop/tools
+cd ~/Desktop/tools
 
-    echo "Checking if $binname is already installed..."
-    if command -v "$binname" &> /dev/null; then
-        echo "$binname is already installed. Skipping."
-        return 0
-    fi
+echo "Installing pdtm..."
+go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
+pdtm -ia
 
-    echo "Downloading $binname..."
-    wget -q "$url" || { echo "Failed to download $binname"; return 1; }
+wget -q https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz
+tar -xvzf kiterunner_1.0.2_linux_amd64.tar.gz
+cp kr /usr/bin
 
-    echo "Extracting $filename..."
-    case "$filename" in
-        *.zip) unzip -q "$filename" ;;
-        *.tgz | *.tar.gz) tar -xvzf "$filename" ;;
-        *) echo "Unsupported file type: $filename"; return 1 ;;
-    esac
+git clone https://github.com/guelfoweb/knock.git
+cd knock
+pip install . --break-system-packages
+cd ..
 
-    echo "Moving $binname to /usr/bin/"
-    mv "$binname" /usr/bin/ || { echo "Failed to move $binname"; return 1; }
-}
+git clone https://github.com/GerbenJavado/LinkFinder.git
+cd LinkFinder
+pip3 install -r requirements.txt --break-system-packages
+python setup.py install
+cd ..
 
-# Array of tools to install (if not available in Kali repo)
-declare -a tools=(
-    "https://github.com/projectdiscovery/asnmap/releases/download/v1.0.4/asnmap_1.0.4_linux_amd64.zip"
-    "https://github.com/projectdiscovery/mapcidr/releases/download/v1.1.2/mapcidr_1.1.2_linux_amd64.zip"
-    "https://github.com/projectdiscovery/subfinder/releases/download/v2.6.0/subfinder_2.6.0_linux_amd64.zip"
-    "https://github.com/owasp-amass/amass/releases/download/v3.23.2/amass_Linux_amd64.zip"
-    "https://github.com/d3mondev/puredns/releases/download/v2.1.1/puredns-Linux-amd64.tgz"
-    "https://github.com/projectdiscovery/naabu/releases/download/v2.1.6/naabu_2.1.6_linux_amd64.zip"
-    "https://github.com/projectdiscovery/httpx/releases/download/v1.3.2/httpx_1.3.2_linux_amd64.zip"
-    "https://github.com/projectdiscovery/katana/releases/download/v1.0.2/katana_1.0.2_linux_amd64.zip"
-    "https://github.com/tomnomnom/waybackurls/releases/download/v0.1.0/waybackurls-linux-amd64-0.1.0.tgz"
-    "https://github.com/projectdiscovery/nuclei/releases/download/v2.9.6/nuclei_2.9.6_linux_amd64.zip"
-    "https://github.com/hahwul/dalfox/releases/download/v2.9.0/dalfox_2.9.0_linux_amd64.tar.gz"
-    "https://github.com/lc/subjs/releases/download/v1.0.1/subjs_1.0.1_linux_amd64.tar.gz"
-    "https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz"
-    "https://github.com/ffuf/ffuf/releases/download/v2.0.0/ffuf_2.0.0_linux_amd64.tar.gz"
-    "https://github.com/tomnomnom/anew/releases/download/v0.1.1/anew-linux-amd64-0.1.1.tgz"
-    "https://github.com/projectdiscovery/dnsx/releases/download/v1.1.4/dnsx_1.1.4_linux_amd64.zip"
-    "https://github.com/projectdiscovery/alterx/releases/download/v0.0.2/alterx_0.0.2_linux_amd64.zip"
-)
+git clone https://github.com/gwen001/related-domains
+cd related-domains
+pip3 install -r requirements.txt --break-system-packages
+cd ..
 
-# Function to install all tools from the array
-install_tools() {
-    echo "Installing tools sequentially..."
-    for tool in "${tools[@]}"; do
-        install_tool "$tool" || { echo "Failed to install $tool"; }
-    done
-}
+git clone https://github.com/blechschmidt/massdns
+cd massdns
+make
+cp massdns /usr/bin
+cd ..
 
-# Function to clone and install Python-based tools
-install_python_tools() {
-    echo "Cloning and installing Python-based tools..."
+git clone https://github.com/devanshbatham/FavFreak
+cd FavFreak
+pip3 install -r requirements.txt --break-system-packages
+cd ..
 
-    declare -a python_tools=(
-        "https://github.com/guelfoweb/knock.git"
-        "https://github.com/blechschmidt/massdns.git"
-        "https://github.com/edoardottt/scilla.git"
-        "https://github.com/LukaSikic/subzy.git"
-        "https://github.com/GerbenJavado/LinkFinder.git"
-    )
+git clone https://github.com/m4ll0k/SecretFinder
+cd SecretFinder
+pip3 install -r requirements.txt --break-system-packages
+cd ..
 
-    for repo in "${python_tools[@]}"; do
-        repo_name=$(basename "$repo" .git)
-        if [ -d "$repo_name" ]; then
-            echo "$repo_name already exists, skipping..."
-            continue
-        fi
-        git clone "$repo" || { echo "Failed to clone $repo"; continue; }
-        cd "$repo_name" || continue
+git clone https://github.com/devanshbatham/ParamSpider
+cd ParamSpider
+pip3 install -r requirements.txt --break-system-packages
+cd ..
 
-        case "$repo" in
-            *"massdns.git"*) make && cp bin/massdns /usr/bin/ ;;
-            *"scilla.git"*) go get && make linux ;;
-            *"subzy.git"*) go build . && cp subzy /usr/bin/ ;;
-            *"LinkFinder.git"*) python3 setup.py install ;;
-        esac
+git clone https://github.com/maurosoria/dirsearch
+cd dirsearch
+pip3 install -r requirements.txt --break-system-packages
+cd ..
 
-        cd .. || exit
-    done
+git clone https://github.com/ozguralp/gmapsapiscanner
+cd gmapsapiscanner
+pip3 install -r requirements.txt --break-system-packages
+cd ..
 
-    pip3 install py-altdns==1.0.2 arjun
-}
+git clone https://github.com/1ndianl33t/Gf-Patterns
 
-# Function to install Go tools using go install
-install_go_tools() {
-    echo "Installing Go tools..."
-    local go_tools=(
-        "github.com/OJ/gobuster/v3@latest"
-        "github.com/tomnomnom/assetfinder@latest"
-        "github.com/ffuf/ffuf@latest"
-        "github.com/tomnomnom/gf@latest"
-        "github.com/tomnomnom/meg@latest"
-        "github.com/tomnomnom/waybackurls@latest"
-        "github.com/LukaSikic/subzy@latest"
-        "github.com/projectdiscovery/asnmap/cmd/asnmap@latest"
-        "github.com/channyein1337/jsleak@latest"
-        "github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest"
-        "github.com/projectdiscovery/dnsx/cmd/dnsx@latest"
-        "github.com/jaeles-project/gospider@latest"
-        "github.com/projectdiscovery/notify/cmd/notify@latest"
-        "github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest"
-        "github.com/projectdiscovery/uncover/cmd/uncover@latest"
-        "github.com/hahwul/dalfox/v2@latest"
-        "github.com/0xsha/GoLinkFinder@latest"
-        "github.com/hakluke/hakrawler@latest"
-        "github.com/edoardottt/csprecon/cmd/csprecon@latest"
-        "github.com/Josue87/gotator@latest"
-        "github.com/j3ssie/osmedeus@latest"
-        "github.com/utkusen/socialhunter@latest"
-        "github.com/003random/getJS@latest"
-        "github.com/tomnomnom/httprobe@latest"
-        "github.com/c-sto/recursebuster@latest"
-        "github.com/lc/gau/v2/cmd/gau@latest"
-        "github.com/iamstoxe/urlgrab@latest"
-        "github.com/mhmdiaa/second-order@latest"
-        "github.com/lc/subjs@latest"
-        "github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest"
-        "github.com/s0md3v/Photon@latest"
-    )
+go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
+go get -u -v github.com/projectdiscovery/dnsprobe
+go get github.com/Ice3man543/SubOver
+go get -u github.com/tomnomnom/gf
+go get -u github.com/tomnomnom/assetfinder
+go get -u -v github.com/projectdiscovery/naabu/v2/cmd/naabu
+go get github.com/tomnomnom/hacks/waybackurls
+go get -u -v github.com/lukasikic/subzy
+go install -v github.com/lukasikic/subzy
+go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+go get github.com/haccer/subjack
+GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+GO111MODULE=on go get -v github.com/projectdiscovery/chaos-client/cmd/chaos
+go get -u -v github.com/hahwul/dalfox
+go get -u -v github.com/lc/gau
+go install github.com/vodafon/waybackrobots@latest
+go get -u github.com/vodafon/waybackrobots
+GO111MODULE=on go get -u -v github.com/lc/subjs
+go get github.com/hakluke/hakcheckurl
+go get github.com/hakluke/hakrawler
+go get github.com/hakluke/hakrevdns
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
+go install github.com/edoardottt/csprecon/cmd/csprecon@latest
+go install github.com/shenwei356/rush@latest
+go get -u github.com/lc/cspparse
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/mhmdiaa/second-order@latest
+go get -u github.com/iamstoxe/urlgrab
+go get -u github.com/c-sto/recursebuster
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install github.com/tomnomnom/httprobe@latest
+go install github.com/utkusen/socialhunter@latest
+go install github.com/003random/getJS/v2@latest
+go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+go install -v github.com/j3ssie/osmedeus@latest
+go install github.com/Josue87/gotator@latest
+go install github.com/hakluke/hakrawler@latest
+go install github.com/0xsha/GoLinkFinder@latest
+go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest
+GO111MODULE=on go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest
+go install github.com/tomnomnom/meg@latest
+go install github.com/tomnomnom/waybackurls@latest
+GO111MODULE=on go install github.com/jaeles-project/gospider@latest
+go install -v github.com/PentestPad/subzy@latest
+go install github.com/channyein1337/jsleak@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go get -u github.com/tomnomnom/assetfinder
+go install github.com/OJ/gobuster/v3@latest
+go install -v github.com/edoardottt/scilla/cmd/scilla@latest
+go install github.com/projectdiscovery/alterx/cmd/alterx@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install -v github.com/tomnomnom/anew@latest
+go install github.com/projectdiscovery/asnmap/cmd/asnmap@latest
+go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/d3mondev/puredns/v2@latest
+go install -v github.com/owasp-amass/amass/v4/...@master
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/hahwul/dalfox/v2@latest
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+GO111MODULE=on go get -u -v github.com/lc/subjs@latest
+go install github.com/gwen001/gitlab-subdomains@latest
 
-    echo "${go_tools[@]}" | xargs -n 1 -P 4 go install || { echo "Failed to install one or more Go tools"; }
-}
-
-# Main execution
-install_packages
-set_go_env
-install_pdtm
-install_tools
-install_python_tools
-install_go_tools
+source ~/.bashrc
 echo "All tools have been installed successfully!"
