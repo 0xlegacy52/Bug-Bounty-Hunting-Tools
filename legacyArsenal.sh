ccd5aa7c2adb399502e30b804baef4674f30d4cc
Abdulrahman muhammad muhammad <100371468+0xlegacy52@users.noreply.github.com>
2024-11-17 11:21:30 +0200

    Update legacyArsenal.sh

--- legacyArsenal.sh
+++ legacyArsenal.sh
@@ -1,157 +1,131 @@
 #!/bin/bash
+
 # Check if the script is run with root privileges
 if [ "$(id -u)" -ne "0" ]; then
     echo "This script must be run as root. Use 'sudo'."
     exit 1
 fi
 
-mkdir -p ~/Desktop/Tools
-mkdir ~/.gf
+# Create necessary directories
+mkdir -p ~/Desktop/Tools ~/.gf
 cd ~/Desktop/Tools
 
+# Update and upgrade installed packages
 echo "Updating package list and upgrading installed packages..."
 apt update && apt upgrade -y && apt full-upgrade -y
 
+# Install necessary packages
 echo "Installing necessary packages..."
-apt-get install -y software-properties-common unzip wget make python3-pip build-essential git ruby-full python3 libpcap-dev cargo dotnet-host netstandard-targeting-pack-2.1 sublist3r assetfinder altdns dirsearch feroxbuster ffuf gobuster dirb dirbuster nmap nikto massdns wpscan dnsrecon jq amass screen ruby
+apt-get install -y software-properties-common unzip wget make python3-pip build-essential git ruby-full python3 libpcap-dev cargo dotnet-host \
+                   netstandard-targeting-pack-2.1 sublist3r assetfinder altdns dirsearch feroxbuster ffuf gobuster dirb dirbuster nmap nikto \
+                   massdns wpscan dnsrecon jq amass screen ruby
+
+# Additional pip installations
 pip3 install arjun corscanner --break-system-packages
 
+# Add Go PPA and install Go
 echo "Adding Go PPA and installing Go..."
 add-apt-repository -y ppa:longsleep/golang-backports
 apt update
 apt install -y golang-go
 
+# Set environment variables for Go
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:/root/go/bin:/$HOME/go/bin
-echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin:/root/go/bin:/$HOME/go/bin" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin:$HOME/go/bin" >> ~/.bashrc
 source ~/.bashrc
 
-
-
+# Install pdtm
 echo "Installing pdtm..."
 go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
 pdtm -ia
 
+# Install kiterunner
+echo "Installing kiterunner..."
 wget -q https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz
 tar -xvzf kiterunner_1.0.2_linux_amd64.tar.gz
 cp kr /usr/bin
 
-git clone https://github.com/guelfoweb/knock.git
-cd knock
-pip install . --break-system-packages
-cd ..
+# Clone and install tools from GitHub with a loop
+tools_to_install=(
+    "guelfoweb/knock"
+    "GerbenJavado/LinkFinder"
+    "gwen001/related-domains"
+    "blechschmidt/massdns"
+    "devanshbatham/FavFreak"
+    "m4ll0k/SecretFinder"
+    "devanshbatham/ParamSpider"
+    "maurosoria/dirsearch"
+    "ozguralp/gmapsapiscanner"
+    "1ndianl33t/Gf-Patterns"
+)
 
-git clone https://github.com/GerbenJavado/LinkFinder.git
-cd LinkFinder
-pip3 install -r requirements.txt --break-system-packages
-python setup.py install
-cd ..
+for tool in "${tools_to_install[@]}"; do
+    git clone https://github.com/"${tool}".git
+    tool_name=$(basename "$tool")
+    cd "$tool_name"
+    if [ -f requirements.txt ]; then
+        pip3 install -r requirements.txt --break-system-packages
+    fi
+    if [ -f setup.py ]; then
+        python3 setup.py install
+    fi
+    cd ..
+done
 
-git clone https://github.com/gwen001/related-domains
-cd related-domains
-pip3 install -r requirements.txt --break-system-packages
-cd ..
+# Install additional Go tools
+go_tools=(
+    "github.com/projectdiscovery/shuffledns/cmd/shuffledns"
+    "github.com/projectdiscovery/dnsprobe"
+    "github.com/Ice3man543/SubOver"
+    "github.com/tomnomnom/gf"
+    "github.com/tomnomnom/assetfinder"
+    "github.com/projectdiscovery/naabu/v2/cmd/naabu"
+    "github.com/tomnomnom/hacks/waybackurls"
+    "github.com/lukasikic/subzy"
+    "github.com/projectdiscovery/nuclei/v2/cmd/nuclei"
+    "github.com/haccer/subjack"
+    "github.com/projectdiscovery/subfinder/v2/cmd/subfinder"
+    "github.com/projectdiscovery/chaos-client/cmd/chaos"
+    "github.com/hahwul/dalfox/v2"
+    "github.com/lc/gau/v2/cmd/gau"
+    "github.com/Emoe/kxss"
+    "github.com/vodafon/waybackrobots"
+    "github.com/lc/subjs"
+    "github.com/hakluke/hakcheckurl"
+    "github.com/hakluke/hakrawler"
+    "github.com/hakluke/hakrevdns"
+    "github.com/projectdiscovery/notify/cmd/notify"
+    "github.com/edoardottt/csprecon/cmd/csprecon"
+    "github.com/shenwei356/rush"
+    "github.com/lc/cspparse"
+    "github.com/mhmdiaa/second-order"
+    "github.com/iamstoxe/urlgrab"
+    "github.com/c-sto/recursebuster"
+    "github.com/projectdiscovery/uncover/cmd/uncover"
+    "github.com/dwisiswant0/crlfuzz/cmd/crlfuzz"
+    "github.com/tomnomnom/meg"
+    "github.com/jaeles-project/gospider"
+    "github.com/PentestPad/subzy"
+    "github.com/channyein1337/jsleak"
+    "github.com/projectdiscovery/dnsx/cmd/dnsx"
+    "github.com/OJ/gobuster/v3"
+    "github.com/edoardottt/scilla/cmd/scilla"
+    "github.com/projectdiscovery/alterx/cmd/alterx"
+    "github.com/projectdiscovery/asnmap/cmd/asnmap"
+    "github.com/projectdiscovery/mapcidr/cmd/mapcidr"
+    "github.com/d3mondev/puredns/v2"
+    "github.com/owasp-amass/amass/v4/..."
+    "github.com/projectdiscovery/httpx/cmd/httpx"
+    "github.com/projectdiscovery/katana/cmd/katana"
+    "github.com/gwen001/gitlab-subdomains"
+)
 
-git clone https://github.com/blechschmidt/massdns
-cd massdns
-make
-cp massdns /usr/bin
-cd ..
+for tool in "${go_tools[@]}"; do
+    go install -v "$tool"@latest
+done
 
-git clone https://github.com/devanshbatham/FavFreak
-cd FavFreak
-pip3 install -r requirements.txt --break-system-packages
-cd ..
-
-git clone https://github.com/m4ll0k/SecretFinder
-cd SecretFinder
-pip3 install -r requirements.txt --break-system-packages
-cd ..
-
-git clone https://github.com/devanshbatham/ParamSpider
-cd ParamSpider
-pip3 install -r requirements.txt --break-system-packages
-cd ..
-
-git clone https://github.com/maurosoria/dirsearch
-cd dirsearch
-pip3 install -r requirements.txt --break-system-packages
-cd ..
-
-git clone https://github.com/ozguralp/gmapsapiscanner
-cd gmapsapiscanner
-pip3 install -r requirements.txt --break-system-packages
-cd ..
-
-git clone https://github.com/1ndianl33t/Gf-Patterns
-
-go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
-go get -u -v github.com/projectdiscovery/dnsprobe
-go get github.com/Ice3man543/SubOver
-go get -u github.com/tomnomnom/gf
-go get -u github.com/tomnomnom/assetfinder
-go get -u -v github.com/projectdiscovery/naabu/v2/cmd/naabu
-go get github.com/tomnomnom/hacks/waybackurls
-go get -u -v github.com/lukasikic/subzy
-go install -v github.com/lukasikic/subzy
-go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-go get github.com/haccer/subjack
-GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
-GO111MODULE=on go get -v github.com/projectdiscovery/chaos-client/cmd/chaos
-go get -u -v github.com/hahwul/dalfox
-go get -u -v github.com/lc/gau
-go get github.com/Emoe/kxss
-go install github.com/vodafon/waybackrobots@latest
-go get -u github.com/vodafon/waybackrobots
-GO111MODULE=on go get -u -v github.com/lc/subjs
-go get github.com/hakluke/hakcheckurl
-go get github.com/hakluke/hakrawler
-go get github.com/hakluke/hakrevdns
-go install -v github.com/projectdiscovery/notify/cmd/notify@latest
-go install github.com/edoardottt/csprecon/cmd/csprecon@latest
-go install github.com/shenwei356/rush@latest
-go get -u github.com/lc/cspparse
-go install github.com/lc/gau/v2/cmd/gau@latest
-go install -v github.com/mhmdiaa/second-order@latest
-go get -u github.com/iamstoxe/urlgrab
-go get -u github.com/c-sto/recursebuster
-go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
-go install github.com/tomnomnom/httprobe@latest
-go install github.com/utkusen/socialhunter@latest
-go install github.com/003random/getJS/v2@latest
-go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
-go install -v github.com/j3ssie/osmedeus@latest
-go install github.com/Josue87/gotator@latest
-go install github.com/hakluke/hakrawler@latest
-go install github.com/0xsha/GoLinkFinder@latest
-go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest
-GO111MODULE=on go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest
-go install github.com/tomnomnom/meg@latest
-go install github.com/tomnomnom/waybackurls@latest
-GO111MODULE=on go install github.com/jaeles-project/gospider@latest
-go install -v github.com/PentestPad/subzy@latest
-go install github.com/channyein1337/jsleak@latest
-go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
-go get -u github.com/tomnomnom/assetfinder
-go install github.com/OJ/gobuster/v3@latest
-go install -v github.com/edoardottt/scilla/cmd/scilla@latest
-go install github.com/projectdiscovery/alterx/cmd/alterx@latest
-go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
-go install -v github.com/tomnomnom/anew@latest
-go install github.com/projectdiscovery/asnmap/cmd/asnmap@latest
-go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install github.com/d3mondev/puredns/v2@latest
-go install -v github.com/owasp-amass/amass/v4/...@master
-go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install github.com/projectdiscovery/katana/cmd/katana@latest
-go install github.com/tomnomnom/waybackurls@latest
-go install github.com/hahwul/dalfox/v2@latest
-go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
-GO111MODULE=on go get -u -v github.com/lc/subjs@latest
-go install github.com/gwen001/gitlab-subdomains@latest
-
+# Source the updated .bashrc file
 source ~/.bashrc
+
+# Confirmation message
 echo "All tools have been installed successfully!"
